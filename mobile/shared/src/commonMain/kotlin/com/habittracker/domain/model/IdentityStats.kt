9f6b2a6b75ef9db4cc37741d2d08800d94c7d3c7
jokot <jokotriyanto12345@gmail.com>
2026-04-30 23:29:38 +0700

    feat(model): IdentityStats + IdentityWithStats

---
+++ mobile/shared/src/commonMain/kotlin/com/habittracker/domain/model/IdentityStats.kt
@@ -0,0 +1,32 @@
+package com.habittracker.domain.model
+
+data class IdentityStats(
+    val identityId: String,
+    val currentStreak: Int,
+    val daysActive: Int,
+    val habitCount: Int,
+    val last14Heat: List<Int>,
+    val last90Heat: List<Int>,
+) {
+    init {
+        require(last14Heat.size == 14) { "last14Heat must have 14 entries" }
+        require(last90Heat.size == 90) { "last90Heat must have 90 entries" }
+    }
+
+    companion object {
+        val Empty = IdentityStats(
+            identityId = "",
+            currentStreak = 0,
+            daysActive = 0,
+            habitCount = 0,
+            last14Heat = List(14) { 0 },
+            last90Heat = List(90) { 0 },
+        )
+        fun emptyFor(identityId: String) = Empty.copy(identityId = identityId)
+    }
+}
+
+data class IdentityWithStats(
+    val identity: Identity,
+    val stats: IdentityStats,
+)
